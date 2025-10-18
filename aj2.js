aaecd1ee208278e46505aade40562c8f8a747b1e
aj305 <beastinmood2@gmail.com>
2025-10-18 23:55:06 +0530

    Initial commit

--- aj2.js
+++ aj2.js
@@ -1,6 +1,1 @@
-import express from 'express'
-const app = express();
-
-app.use('/media', express.static('upload'));
-
-app.listen(3000, () => console.log('Server running'));
+console.log("this is aj2");
